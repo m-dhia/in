f09a2da80615f0bf40278757632c7cfe93411c06
Dhia <147658538+m-dhia@users.noreply.github.com>
2024-03-12 11:18:06 +0100

    Create in.sh

---
+++ in.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Update package database and install packages
+paru -Sy picom-git wezterm rofi acpi acpid acpi_call upower lxappearance-gtk3 \
+jq inotify-tools polkit-gnome xdotool xclip gpick ffmpeg blueman redshift \
+pipewire pipewire-alsa pipewire-pulse alsa-utils brightnessctl feh maim \
+mpv mpd mpc mpdris2 python-mutagen ncmpcpp playerctl --needed
+
+# Enable and start services
+systemctl --user enable mpd.service
+systemctl --user start mpd.service
+
+echo "Installation and service setup completed."
